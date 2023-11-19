5aec765f617b46e081056a7cfc7cbf51087e93d4
Darren <darren.daley@hotmail.co.uk>
2023-11-19 17:00:35 +0000

    created one header file to reference em all

---
+++ include/karakuri.h
@@ -0,0 +1,23 @@
+#ifndef KARAKURI_H
+#define KARAKURI_H
+
+#include "api.h"
+#include "buttons.h"
+#include "circle.h"
+#include "colour.h"
+#include "game.h"
+#include "gamepad.h"
+#include "graphics_device.h"
+#include "keyboard.h"
+#include "keys.h"
+#include "matrix.h"
+#include "mouse.h"
+#include "rectangle.h"
+#include "song.h"
+#include "sound_effect.h"
+#include "sprite_renderer.h"
+#include "texture.h"
+#include "vector2.h"
+#include "vector3.h"
+
+#endif // !KARAKURI_H
